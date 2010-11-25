a8904b5afe52d4127d9a5daa90ecad85aa3b1c79
Jack Dempsey <jack.dempsey@gmail.com>
2010-11-25 09:49:00 -0800

    add in some custom aliases

--- lib/aliases.zsh
+++ lib/aliases.zsh
@@ -9,8 +9,6 @@ alias -- -='cd -'
 # Super user
 alias _='sudo'
 
-#alias g='grep -in'
-
 # Show history
 alias history='fc -l 1'
 
@@ -21,3 +19,13 @@ alias ll='ls -l'
 alias sl=ls # often screw this up
 
 alias afind='ack-grep -il'
+
+
+alias bi='bundle install'
+alias ea='vim ~/.oh-my-zsh/lib/aliases.zsh'
+alias sl='. ~/.oh-my-zsh/lib/aliases.zsh'
+alias gi='gem install'
+alias gh='nocorrect gh'
+alias gr='grep -riH'
+alias pror='nocorrect pror'
+alias sc='script/rails c'
