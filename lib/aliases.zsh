ecb22b141750fe34dfe4f7906bfcf227e5556ffe
Jack Dempsey <jack.dempsey@gmail.com>
2010-12-15 18:40:12 -0800

    more aliases

--- lib/aliases.zsh
+++ lib/aliases.zsh
@@ -11,6 +11,7 @@ alias _='sudo'
 
 # Show history
 alias history='fc -l 1'
+alias h='history'
 
 # List direcory contents
 alias lsa='ls -lah'
@@ -23,9 +24,21 @@ alias afind='ack-grep -il'
 
 alias bi='bundle install'
 alias ea='vim ~/.oh-my-zsh/lib/aliases.zsh'
+alias eag='vim ~/.oh-my-zsh/plugins/git/git.plugin.zsh'
 alias sl='. ~/.oh-my-zsh/lib/aliases.zsh'
 alias gi='gem install'
 alias gh='nocorrect gh'
 alias gr='grep -riH'
 alias pror='nocorrect pror'
 alias sc='script/rails c'
+
+#alias ns='/opt/nginx/sbin/nginx'
+#alias nr='/opt/nginx/sbin/nginx'
+
+alias rdm='rake db:migrate'
+alias gui='gem uninstall'
+alias ar='sudo apachectl restart'
+alias tir='nocorrect tir'
+alias f='fossil'
+alias v='vagrant'
+alias orch='nocorrect orch'
